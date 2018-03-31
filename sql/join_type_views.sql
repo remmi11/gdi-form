1b373c4fe78aa20c451c5d7272f6d40be1e1dd6d
remmi11 <wtgeographer@gmail.com>
2018-03-31 15:46:23 -0500

    make it better

---
+++ sql/join_type_views.sql
@@ -0,0 +1,4 @@
+﻿SELECT DISTINCT join_type FROM master_geom;
+SELECT * FROM master_geom WHERE join_type = 'prad' LIMIT 100;
+SELECT * FROM master_geom WHERE join_type = 'rural' LIMIT 100;
+SELECT * FROM master_geom WHERE join_type = 'plss' LIMIT 100;
